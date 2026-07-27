283be2ccb0d00fe5e13c4ba9cccbcf4a7fc69cc8
Juro <contact@jrothkopf.dev>
2026-07-27 09:02:14 +0200

    Remove floating credits banner

--- Scenes/TitleScene.js
+++ Scenes/TitleScene.js
@@ -17,9 +17,6 @@ export default class TitleScene extends Phaser.Scene {
 	this.instrText1 = this.add.text(0, 0, 'Control phaser with LEFT and RIGHT keys.', { fontSize: '30px', fill: '#fff' }, {fontFamily: 'Lucida Console, Monaco, monospace'});
 	this.instrText2 = this.add.text(0, 0, 'Press SPACE to shoot.', { fontSize: '30px', fill: '#fff' }, {fontFamily: 'Lucida Console, Monaco, monospace'});
 	this.instrText3 = this.add.text(0, 0, 'When ready, press SPACE to play!', { fontSize: '30px', fill: '#fff' }, {fontFamily: 'Lucida Console, Monaco, monospace'});
-	this.credtext = this.add.text(0, 0, 'Credits (clickable): dev.jurolytics.blog', { fontSize: '15px', fill: '#fff', wordWrap: {width: 3} }, {fontFamily: 'Lucida Console, Monaco, monospace'},)
-	.setInteractive().on('pointerdown', function () {window.location.href = "http://dev.jurolytics.blog/";﻿ }, this);
-
 
 	this.zone = this.add.zone(config.width/2, config.height/2, config.width, config.height);
  
@@ -48,78 +45,11 @@ export default class TitleScene extends Phaser.Scene {
   		this.zone
 	);
 
-	Phaser.Display.Align.In.Center(
-  		this.credtext,
-  		this.zone
-	);
-
 	this.titleText.setY(200);
 	this.banText.setY(300);
 	this.instrText1.setY(400);
 	this.instrText2.setY(450);
 	this.instrText3.setY(500);
-	this.credtext.setY(750);
-
-	this.timeline = this.tweens.timeline({
-
-        targets: this.credtext,
-        loop: -1,
-        
-        tweens: [
-        {
-            x: 1000,
-            ease: 'Linear',
-            duration: 1000
-        },
-        {
-            y: 50,
-            ease: 'Linear',
-            duration: 3000,
-            onUpdate: ()=>{
-
-        	// let col = Phaser.Display.Color.Interpolate.ColorWithColor('#ffffff', '#F02929', 5000, 0);
-        	// let colourInt = Phaser.Display.Color.GetColor(col.r, col.g, col.b);
-        	this.credtext.setStyle({color: '#0000FF'});
-
-    	}
-        },
-        {
-            x: 50,
-            ease: 'Linear',
-            duration: 2000,
-            onUpdate: ()=>{
-
-        	this.credtext.setStyle({color: '#439c3f'});
-
-    	}
-        },
-        {
-            y: 750,
-            ease: 'Linear',
-            duration: 3000,
-            onUpdate: ()=>{
-
-        	this.credtext.setStyle({color: '#FFFF00'});
-
-    	}
-        },
-        {
-            x: 510,
-            ease: 'Linear',
-            duration: 1000,
-            onUpdate: ()=>{
-
-        	this.credtext.setStyle({color: '#ffa700'});
-
-    	}
-        }
-        ]
-
-    });
-
-    // console.log(this.credtext.x);
-
-
 
 	// Cursors
     this.cursors = this.input.keyboard.createCursorKeys();
@@ -128,27 +58,9 @@ export default class TitleScene extends Phaser.Scene {
 
   update () {
 
-  	// this.credtext.x += 5;
-
   	if (this.cursors.space.isDown) {
         this.scene.start('Game');
     }
-
-   //  if (this.credtext.x === 750 && this.credtext.y === 750) {
-  	// 	this.credtext.y -= 5;
-  	// }
-
-  	// if (this.credtext.x === 750 && this.credtext.y === 50) {
-  	// 	this.credtext.x -= 5;
-  	// }
-
-  	// if (this.credtext.x === 50 && this.credtext.y === 50) {
-  	// 	this.credtext.y += 5;
-  	// }
-
-  	// if (this.credtext.x === 50 && this.credtext.y === 750) {
-  	// 	this.credtext.x += 5;
-  	// }
 
   }
  
